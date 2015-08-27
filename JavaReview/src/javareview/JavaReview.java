1058b902d02a5ce559e8ad923cb2202269cd5355
shoegit <ngsm@help.edu.my>
2015-08-27 14:12:57 +0800

    calculating pay for worker

--- JavaReview/src/javareview/JavaReview.java
+++ JavaReview/src/javareview/JavaReview.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package javareview;
-
+import java.util.Scanner;
 /**
  *
  * @author ngsm
@@ -18,6 +18,29 @@ public class JavaReview {
         // TODO code application logic here
         System.out.println("Hello From BIT203");
         System.out.println("Now I know how to git");
+        
+        // one and a half the normal rate for 
+        // any extra hour worked greater than 40.
+        final int NORMALHOURS = 50;
+        final double OVERTIMERATE = 0.5;
+        
+        Scanner kbd = new Scanner(System.in);
+        System.out.print("Enter hourly rate :");
+        
+        double hourlyRate = kbd.nextDouble();
+        System.out.print("Enter hours worked :");
+        int hoursWorked = kbd.nextInt();
+        double weeklyWage;
+        if (hoursWorked > NORMALHOURS)
+        {
+            double normalPay = NORMALHOURS * hourlyRate;
+            double overtimePay = (hoursWorked - NORMALHOURS) * (OVERTIMERATE * hourlyRate);
+            weeklyWage = normalPay + overtimePay;
+        }
+        else
+             weeklyWage = hourlyRate * hoursWorked;
+        System.out.println("The weekly wage is " + weeklyWage);
+        
     }
     
 }
