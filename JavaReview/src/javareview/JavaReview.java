4f56135f2fb262927f330d476223ace36fe8ab13
shoegit <ngsm@help.edu.my>
2015-08-27 14:41:31 +0800

    calculating pay for worker

--- JavaReview/src/javareview/JavaReview.java
+++ JavaReview/src/javareview/JavaReview.java
@@ -11,36 +11,57 @@ import java.util.Scanner;
  */
 public class JavaReview {
 
-    /**
-     * @param args the command line arguments
-     */
+    
+     // one and a half the normal rate for 
+      // any extra hour worked greater than 40.
+       static final int NORMALHOURS = 50;
+       static final double OVERTIMERATE = 0.5;
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello From BIT203");
-        System.out.println("Now I know how to git");
         
-        // one and a half the normal rate for 
-        // any extra hour worked greater than 40.
-        final int NORMALHOURS = 50;
-        final double OVERTIMERATE = 0.5;
+        double total = 0;
         
-        Scanner kbd = new Scanner(System.in);
-        System.out.print("Enter hourly rate :");
-        
-        double hourlyRate = kbd.nextDouble();
-        System.out.print("Enter hours worked :");
-        int hoursWorked = kbd.nextInt();
-        double weeklyWage;
-        if (hoursWorked > NORMALHOURS)
+        for (int i = 0; i < 5; i++)
         {
-            double normalPay = NORMALHOURS * hourlyRate;
-            double overtimePay = (hoursWorked - NORMALHOURS) * (OVERTIMERATE * hourlyRate);
-            weeklyWage = normalPay + overtimePay;
-        }
-        else
-             weeklyWage = hourlyRate * hoursWorked;
-        System.out.println("The weekly wage is " + weeklyWage);
+            // Enter for one worker
+            Scanner kbd = new Scanner(System.in);
+            
+            System.out.print("Enter worker name :");
+            String name = kbd.nextLine();
+            System.out.print("Enter hourly rate :");
+
+            double hourlyRate = kbd.nextDouble();
+            System.out.print("Enter hours worked :");
+            int hoursWorked = kbd.nextInt();
+            kbd.nextLine();
+            double weeklyWage =  wageCalculator(hourlyRate, hoursWorked);
+            total += weeklyWage;
+            displayWages(name, weeklyWage);
+         } // end of for loop
         
+        System.out.println("The total wages paid is " + total); 
     }
     
+    /* Method to calculate the weekly wage
+        */
+    public static double wageCalculator(double rate, int hours)
+    {
+        double weeklyWage;
+            if (hours > NORMALHOURS)
+            {
+                double normalPay = NORMALHOURS * rate;
+                double overtimePay = (hours - NORMALHOURS) * (OVERTIMERATE * rate);
+                weeklyWage = normalPay + overtimePay;
+            }
+            else
+                 weeklyWage = rate * hours;
+    
+              return weeklyWage;
+    }
+    
+    public static void displayWages(String name, double pay)
+    {
+            System.out.printf("The weekly wage for is %s is %.2f\n", name,pay);
+       
+    }
 }
