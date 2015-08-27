d819b5374b1f6d2bbbbcd653b3418331a275150b
shoegit <ngsm@help.edu.my>
2015-08-27 14:55:39 +0800

    calculating pay for worker using constructor

--- JavaReview/src/javareview/JavaReview.java
+++ JavaReview/src/javareview/JavaReview.java
@@ -19,24 +19,31 @@ public class JavaReview {
     
     public static void main(String[] args) {
         
+        //declare array of 5 workers
+        
+        Worker[] w = new Worker[5];
         double total = 0;
         
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < w.length; i++)
         {
             // Enter for one worker
             Scanner kbd = new Scanner(System.in);
             
-            System.out.print("Enter worker name :");
+            System.out.print("Enter name for worker  :" + (i+1));
             String name = kbd.nextLine();
+            
             System.out.print("Enter hourly rate :");
-
-            double hourlyRate = kbd.nextDouble();
+            double rate = kbd.nextDouble();
             System.out.print("Enter hours worked :");
-            int hoursWorked = kbd.nextInt();
+       
             kbd.nextLine();
-            double weeklyWage =  wageCalculator(hourlyRate, hoursWorked);
+            int hours = kbd.nextInt();
+            
+            w[i] = new Worker(name, rate, hours);
+            
+            double weeklyWage =  wageCalculator(w[i]);
             total += weeklyWage;
-            displayWages(name, weeklyWage);
+            displayWages(w[i].getName(), weeklyWage);
          } // end of for loop
         
         System.out.println("The total wages paid is " + total); 
@@ -44,17 +51,17 @@ public class JavaReview {
     
     /* Method to calculate the weekly wage
         */
-    public static double wageCalculator(double rate, int hours)
+    public static double wageCalculator(Worker employee)
     {
         double weeklyWage;
-            if (hours > NORMALHOURS)
+            if (employee.getHoursWorked() > NORMALHOURS)
             {
-                double normalPay = NORMALHOURS * rate;
-                double overtimePay = (hours - NORMALHOURS) * (OVERTIMERATE * rate);
+                double normalPay = NORMALHOURS * employee.getHourlyRate();
+                double overtimePay = (employee.getHoursWorked() - NORMALHOURS) * (OVERTIMERATE * employee.getHourlyRate());
                 weeklyWage = normalPay + overtimePay;
             }
             else
-                 weeklyWage = rate * hours;
+                 weeklyWage = employee.getHourlyRate() * employee.getHoursWorked();
     
               return weeklyWage;
     }
